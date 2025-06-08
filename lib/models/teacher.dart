d81045358891786ec1310591b733f63b958c63e3
nailsonseat <aadarshv3@gmail.com>
2025-06-08 13:19:40 +0530

    update teacher model

--- lib/models/teacher.dart
+++ lib/models/teacher.dart
@@ -1,24 +1,21 @@
 class Teacher {
-  String id;
-  String name;
-  String email;
-  String phone;
+  final String id;
+  final String name;
+  final String email;
+  final String? phone;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final bool isActive;
 
   Teacher({
     required this.id,
     required this.name,
     required this.email,
     required this.phone,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.isActive,
   });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-      'phone': phone,
-    };
-  }
 
   factory Teacher.fromJson(Map<String, dynamic> json) {
     return Teacher(
@@ -26,7 +23,21 @@ class Teacher {
       name: json['name'],
       email: json['email'],
       phone: json['phone'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      isActive: json['isActive'],
     );
   }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'isActive': isActive,
+    };
+  }
 }
