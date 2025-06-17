82115789ef58ddaebb61a21ce7ba8a3bb9c5c658
nailsonseat <aadarshv3@gmail.com>
2025-06-17 22:20:51 +0530

    update assignment bloc

--- lib/blocs/assignment/assignment_bloc.dart
+++ lib/blocs/assignment/assignment_bloc.dart
@@ -2,50 +2,123 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import '../../models/assignments.dart';
+import '../../models/assignment_extended.dart';
 import '../../repositories/assignment_repository.dart';
 
 part 'assignment_event.dart';
 part 'assignment_state.dart';
 
-class AssignmentBloc extends Bloc<AssignmentEvent, PagingState<int, Assignment>> {
+class AssignmentBloc extends Bloc<AssignmentEvent, AssignmentState> {
   final AssignmentRepository repository;
 
-  AssignmentBloc({required this.repository}) : super(PagingState()) {
+  AssignmentBloc({required this.repository}) : super(AssignmentInitial()) {
     on<FetchNextAssignmentPage>((event, emit) async {
-      final state = this.state;
-      if (state.isLoading || !(state.hasNextPage)) return;
-
-      emit(state.copyWith(isLoading: true, error: null));
-
-      try {
-        final int nextOffset = (state.keys?.last ?? 0) + (state.pages?.lastOrNull?.length ?? 0);
-        final response = await repository.getAssignments(
-          teacherId: event.teacherId,
-          limit: 10,
-          offset: 0,
-        );
+      AssignmentState currentState = state;
+      if (event.extended) {
+        PagingState<int, AssignmentExtended> pagingState = currentState is AssignmentExtendedSuccess
+            ? currentState.pagingState
+            : PagingState<int, AssignmentExtended>();
 
 
-        if (response.statusCode == 200) {
-          final List<Assignment> assignments = [];
-          for (var item in response.data) {
-            assignments.add(Assignment.fromJson(item));
-          }
-          final isLastPage = assignments.length < event.pageSize;
+         emit(AssignmentExtendedSuccess(pagingState.copyWith(isLoading: true)));
 
-          emit(
-            state.copyWith(
-              pages: [...?state.pages, assignments],
-              keys: [...state.keys ?? [], nextOffset],
-              hasNextPage: !isLastPage,
-              isLoading: false,
-            ),
+        try {
+          final int nextOffset = (pagingState.keys?.last ?? 0) + (pagingState.pages?.lastOrNull?.length ?? 0);
+          final response = await repository.getAssignments(
+            teacherId: event.teacherId,
+            limit: event.pageSize,
+            offset: nextOffset,
+            extended: true,
           );
+
+          if (response.statusCode == 200) {
+            final List<AssignmentExtended> assignments = [];
+            for (var item in response.data) {
+              assignments.add(AssignmentExtended.fromJson(item));
+            }
+            final isLastPage = assignments.length < event.pageSize;
+
+            emit(
+              AssignmentExtendedSuccess(
+                pagingState.copyWith(
+                  pages: [...?pagingState.pages, assignments],
+                  keys: [...?pagingState.keys, nextOffset],
+                  hasNextPage: !isLastPage,
+                  isLoading: false,
+                ),
+              ),
+            );
+          } else {
+            emit(AssignmentFailure(Exception('Failed to load assignments')));
+          }
+        } catch (error) {
+          emit(AssignmentFailure(error));
+        }
+      } else {
+        PagingState<int, Assignment> pagingState = currentState is AssignmentOriginalSuccess
+            ? currentState.pagingState
+            : PagingState<int, Assignment>();
+        
+        emit(AssignmentOriginalSuccess(pagingState.copyWith(isLoading: true)));
+
+        try {
+          final int nextOffset = (pagingState.keys?.last ?? 0) + (pagingState.pages?.lastOrNull?.length ?? 0);
+          final response = await repository.getAssignments(
+            teacherId: event.teacherId,
+            limit: event.pageSize,
+            offset: nextOffset,
+            extended: false,
+          );
+
+          if (response.statusCode == 200) {
+            final List<Assignment> assignments = [];
+            for (var item in response.data) {
+              assignments.add(Assignment.fromJson(item));
+            }
+            final isLastPage = assignments.length < event.pageSize;
+
+            emit(
+              AssignmentOriginalSuccess(
+                pagingState.copyWith(
+                  pages: [...?pagingState.pages, assignments],
+                  keys: [...?pagingState.keys, nextOffset],
+                  hasNextPage: !isLastPage,
+                  isLoading: false,
+                ),
+              ),
+            );
+          } else {
+            emit(AssignmentFailure(Exception('Failed to load assignments')));
+          }
+        } catch (error) {
+          emit(AssignmentFailure(error));
+        }
+      }
+    });
+    on<InitializeAssignmentPaging>((event, emit) {
+      if (event.extended) {
+        emit(AssignmentExtendedSuccess(PagingState<int, AssignmentExtended>(isLoading: true)));
+      } else {
+        emit(AssignmentOriginalSuccess(PagingState<int, Assignment>(isLoading: true)));
+      }
+    });
+    on<FetchAssignmentById>((event, emit) async {
+      emit(AssignmentSingleLoading());
+      try {
+        final response = await repository.getAssignment(id: event.id, extended: event.extended);
+        if (response.statusCode == 200) {
+          if (event.extended) {
+            final assignment = AssignmentExtended.fromJson(response.data);
+            emit(AssignmentSingleExtendedSuccess(assignment));
+          } else {
+            final assignment = Assignment.fromJson(response.data);
+            emit(AssignmentSingleOriginalSuccess(assignment));
+          }
         } else {
-          emit(state.copyWith(error: Exception('Failed to load assignments'), isLoading: false));
+          emit(AssignmentSingleFailure(Exception('Failed to load assignment')));
         }
       } catch (error) {
-        emit(state.copyWith(error: error, isLoading: false));
+        emit(AssignmentSingleFailure(error));
       }
     });
   }
--- lib/blocs/assignment/assignment_state.dart
+++ lib/blocs/assignment/assignment_state.dart
@@ -1,4 +1,41 @@
 part of 'assignment_bloc.dart';
 
-// PagingState will be used directly as the bloc state.
+@immutable
+abstract class AssignmentState {}
 
+class AssignmentInitial extends AssignmentState {}
+
+class AssignmentOriginalSuccess extends AssignmentState {
+  final PagingState<int, Assignment> pagingState;
+
+  AssignmentOriginalSuccess(this.pagingState);
+}
+
+class AssignmentExtendedSuccess extends AssignmentState {
+  final PagingState<int, AssignmentExtended> pagingState;
+
+  AssignmentExtendedSuccess(this.pagingState);
+}
+
+class AssignmentFailure extends AssignmentState {
+  final Object error;
+  AssignmentFailure(this.error);
+}
+
+// New states for fetching a single assignment
+class AssignmentSingleLoading extends AssignmentState {}
+
+class AssignmentSingleOriginalSuccess extends AssignmentState {
+  final Assignment assignment;
+  AssignmentSingleOriginalSuccess(this.assignment);
+}
+
+class AssignmentSingleExtendedSuccess extends AssignmentState {
+  final AssignmentExtended assignment;
+  AssignmentSingleExtendedSuccess(this.assignment);
+}
+
+class AssignmentSingleFailure extends AssignmentState {
+  final Object error;
+  AssignmentSingleFailure(this.error);
+}
