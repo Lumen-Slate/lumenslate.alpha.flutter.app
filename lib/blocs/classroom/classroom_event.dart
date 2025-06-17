23557f728ee5e355f9689c4df52dd0a4fccbece1
nailsonseat <aadarshv3@gmail.com>
2025-06-17 14:51:02 +0530

    classroom bloc

---
+++ lib/blocs/classroom/classroom_event.dart
@@ -0,0 +1,22 @@
+part of 'classroom_bloc.dart';
+
+@immutable
+sealed class ClassroomEvent {}
+
+class LoadClassrooms extends ClassroomEvent {
+  final String teacherId;
+  final int limit;
+  final int offset;
+
+  LoadClassrooms({
+    required this.teacherId,
+    this.limit = 10,
+    this.offset = 0,
+  });
+}
+
+class RefreshClassrooms extends ClassroomEvent {
+  final String teacherId;
+
+  RefreshClassrooms({required this.teacherId});
+}
