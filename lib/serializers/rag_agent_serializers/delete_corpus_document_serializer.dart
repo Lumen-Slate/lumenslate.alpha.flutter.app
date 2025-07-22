053fe9031f96f3eda61969c848bf4d11c17950e5
nailsonseat <aadarshv3@gmail.com>
2025-07-22 22:50:47 +0530

    update old serializers

--- lib/serializers/rag_agent_serializers/delete_corpus_document_serializer.dart
+++ lib/serializers/rag_agent_serializers/delete_corpus_document_serializer.dart
@@ -1,40 +1,28 @@
 // lib/serializers/rag_agent_serializers/delete_corpus_document_serialzier.dart
 class DeleteCorpusDocumentSerializer {
-  final String corpusName;
-  final String deletedFileName;
-  final bool documentDeleted;
+  final String displayName;
+  final String documentId;
   final String message;
-  final String operationName;
-  final String status;
 
   DeleteCorpusDocumentSerializer({
-    required this.corpusName,
-    required this.deletedFileName,
-    required this.documentDeleted,
+    required this.displayName,
+    required this.documentId,
     required this.message,
-    required this.operationName,
-    required this.status,
   });
 
   factory DeleteCorpusDocumentSerializer.fromJson(Map<String, dynamic> json) {
     return DeleteCorpusDocumentSerializer(
-      corpusName: json['corpusName'],
-      deletedFileName: json['deletedFileName'],
-      documentDeleted: json['documentDeleted'],
+      displayName: json['displayName'],
+      documentId: json['documentId'],
       message: json['message'],
-      operationName: json['operationName'],
-      status: json['status'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'corpusName': corpusName,
-      'deletedFileName': deletedFileName,
-      'documentDeleted': documentDeleted,
+      'displayName': displayName,
+      'documentId': documentId,
       'message': message,
-      'operationName': operationName,
-      'status': status,
     };
   }
 }
