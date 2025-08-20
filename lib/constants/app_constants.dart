57446c4139e339e90fcf424d5fd7646753f48e0f
SHUBHAM SHARMA <shubham27.sharma03@gmail.com>
2025-08-21 00:15:49 +0530

    Update backend domain URL in app constants
    Changed the backendDomain constant to point to a new deployment URL and updated the commented localhost reference for clarity.

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,8 +2,9 @@ class AppConstants {
   static const String appName = 'Lumen Slate';
   static const double desktopScaleWidth = 1960;
   static const double mobileScaleWidth = 450;
-  // static const String backendDomain = 'http://0.0.0.0:8080';
-  static const String backendDomain = 'https://lumenslate-backend-756147067348.asia-south1.run.app';
+  // static const String backendDomain = 'http://localhost:8080';
+  static const String backendDomain =
+      'https://lumenslate-backend-967348249814.asia-south1.run.app';
 
   static Duration otpTimeOutDuration = const Duration(minutes: 2);
 }
