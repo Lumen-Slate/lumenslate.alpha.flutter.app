fe0958c434ed1cef539e792a6a783ae4b27e31ed
nailsonseat <aadarshv3@gmail.com>
2025-08-23 23:26:14 +0530

    changed domain

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,8 +3,8 @@ class AppConstants {
   static const double desktopScaleWidth = 1960;
   static const double mobileScaleWidth = 450;
 
-  // static const String backendDomain = 'http://localhost:8080';
-  static const String backendDomain = 'http://34.72.222.77/api/v1';
+  // static const String backendDomain = 'http://localhost:8080/api/v1';
+  static const String backendDomain = 'http://34.31.238.121/api/v1';
 
   static Duration otpTimeOutDuration = const Duration(minutes: 2);
 }
