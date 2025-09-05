dbd31c54fbf348a7edc58c63e466ea4664d3e400
nailsonseat <aadarshv3@gmail.com>
2025-09-05 18:58:55 +0530

    update entire auth bloc to handle separate roles

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -3,8 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
+import 'package:lumen_slate/repositories/user_repository.dart';
 
+import '../../models/lumen_user.dart';
+import '../../models/students.dart';
 import '../../models/teacher.dart';
+import '../../repositories/student_repository.dart';
 import '../../repositories/teacher_repository.dart';
 import '../../services/google_auth_services.dart';
 import '../../services/phone_auth_services.dart';
@@ -16,6 +20,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final GoogleAuth googleAuthServices;
   final PhoneAuth phoneAuthServices;
   final TeacherRepository teacherRepository;
+  final UserRepository userRepository = UserRepository();
+  final StudentRepository studentRepository = StudentRepository();
   final _logger = Logger();
 
   AuthBloc({required this.googleAuthServices, required this.phoneAuthServices, required this.teacherRepository})
@@ -25,53 +31,113 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       try {
         Map<String, dynamic>? response = await googleAuthServices.signInGoogle();
         if (response == null) {
-          emit(AuthPending());
+          emit(AuthNotSignedIn());
           return;
         }
 
-        final checkTeacherResponse = await teacherRepository.getAllTeachers({
+        final checkUserResponse = await userRepository.getAllUsers({
           "email": response['email'],
-          "limit": "10",
+          "limit": "1",
           "offset": "0",
         });
 
-        Teacher? teacher;
+        LumenUser? lumenUser;
 
-        if (checkTeacherResponse.data == null) {
+        if (checkUserResponse.data == null) {
           try {
-            final createTeacherResponse = await teacherRepository.createTeacher(
-              // Teacher(id: response['id'], name: response['displayName'], email: response['email']),
+            final createUserResponse = await userRepository.createUser(
               {"name": response['displayName'], "email": response['email']},
             );
-            if (createTeacherResponse.statusCode! >= 200) {
-              teacher = Teacher.fromJson(createTeacherResponse.data);
+            if (createUserResponse.statusCode! >= 200) {
+              lumenUser = LumenUser.fromJson({...createUserResponse.data, 'photoUrl': response['photoUrl']});
             } else {
-              _logger.e('Failed to create teacher: ${createTeacherResponse.data}');
+              _logger.e('Failed to create user: ${createUserResponse.data}');
+              emit(AuthNotSignedIn());
+              return;
             }
           } catch (e) {
-            _logger.e('Error creating teacher: $e');
+            _logger.e('Error creating user: $e');
+            emit(AuthNotSignedIn());
+            return;
           }
         } else {
-          teacher = Teacher.fromJson(checkTeacherResponse.data[0]);
+          lumenUser = LumenUser.fromJson({...checkUserResponse.data[0], 'photoUrl': response['photoUrl']});
         }
 
-        if (teacher == null) {
-          throw Exception('Failed to fetch teacher');
-        }
 
-        emit(
-          AuthSuccess(
-            id: response['id'],
-            displayName: response['displayName'],
-            email: response['email'],
-            photoUrl: response['photoUrl'],
-            message: 'User is logged in. ${response['displayName']}',
-            teacher: teacher,
-          ),
-        );
+        if (lumenUser.role == null) {
+          Logger().w('User role is null for user: ${lumenUser.id}');
+          emit(AuthSignedInAsAnonymous(lumenUser));
+          return;
+        } else if (lumenUser.role == 'teacher') {
+          final checkTeacherResponse = await teacherRepository.getAllTeachers({
+            "email": response['email'],
+            "limit": "10",
+            "offset": "0",
+          });
+          Teacher? teacher;
+
+          if (checkTeacherResponse.data == null) {
+            try {
+              final createTeacherResponse = await teacherRepository.createTeacher(
+                {"name": response['displayName'], "email": response['email']},
+              );
+              if (createTeacherResponse.statusCode! >= 200) {
+                teacher = Teacher.fromJson(createTeacherResponse.data);
+              } else {
+                _logger.e('Failed to create teacher: ${createTeacherResponse.data}');
+                emit(AuthNotSignedIn());
+                return;
+              }
+            } catch (e) {
+              _logger.e('Error creating teacher: $e');
+              emit(AuthNotSignedIn());
+              return;
+            }
+          } else {
+            teacher = Teacher.fromJson(checkTeacherResponse.data[0]);
+          }
+          emit(AuthSignedInAsTeacher(user: lumenUser, teacher: teacher));
+          return;
+        } else if (lumenUser.role == 'student') {
+          final checkStudentResponse = await studentRepository.getStudents({
+            "email": response['email'],
+            "limit": "10",
+            "offset": "0",
+            'extended': 'true',
+          });
+
+          Student? student;
+
+          if (checkStudentResponse.data == null) {
+            try {
+              final createStudentResponse = await studentRepository.createStudent(
+                {"name": response['displayName'], "email": response['email']},
+              );
+              if (createStudentResponse.statusCode! >= 200) {
+                student = Student.fromJson(createStudentResponse.data);
+              } else {
+                _logger.e('Failed to create student: ${createStudentResponse.data}');
+                emit(AuthNotSignedIn());
+                return;
+              }
+            } catch (e) {
+              _logger.e('Error creating student: $e');
+              emit(AuthNotSignedIn());
+              return;
+            }
+          } else {
+            student = Student.fromJson(checkStudentResponse.data[0]);
+          }
+          emit(AuthSignedInAsStudent(user: lumenUser, student: student));
+          return;
+        } else {
+          emit(AuthNotSignedIn());
+          return;
+        }
       } catch (e) {
         Logger().e(e);
-        emit(AuthPending());
+        emit(AuthNotSignedIn());
         return;
       }
     });
@@ -88,64 +154,20 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       final user = await userCompleter.future.timeout(const Duration(seconds: 2), onTimeout: () => null);
 
       if (user != null) {
-        final checkTeacherResponse = await teacherRepository.getAllTeachers({
-          "email": user.email!,
-          "limit": "10",
-          "offset": "0",
-        });
-        Teacher? teacher;
-
-        if (checkTeacherResponse.data == null) {
-          emit(AuthPending());
-        } else {
-          teacher = Teacher.fromJson(checkTeacherResponse.data[0]);
+        final checkUserResponse = await userRepository.getAllUsers({"email": user.email!, "limit": "1", "offset": "0"});
+        if (checkUserResponse.data == null) {
+          emit(AuthNotSignedIn());
+          return;
         }
-
-        if (teacher == null) {
-          throw Exception('Failed to fetch teacher');
-        }
-
-        emit(
-          AuthSuccess(
-            id: user.uid,
-            displayName: user.displayName ?? '',
-            email: user.email ?? '',
-            photoUrl: user.photoURL,
-            message: 'User is logged in. ${user.displayName}',
-            teacher: teacher,
-          ),
-        );
         event.onSuccess?.call();
       } else {
-        emit(AuthPending());
+        emit(AuthNotSignedIn());
       }
     });
 
-    // on<OTPSignIn>((event, emit) {
-    //   emit(Loading());
-    //   try {
-    //     final response = phoneAuthServices.signInOTP(event.countryCode, event.phoneNumber);
-    //     _logger.i(response);
-    //     emit(AuthSuccess(id: '', displayName: '', email: '', message: 'OTP sent to ${event.phoneNumber}'));
-    //   } catch (e) {
-    //     emit(AuthFailure(e.toString()));
-    //   }
-    // });
-    //
-    // on<OTPVerify>((event, emit) {
-    //   emit(Loading());
-    //   try {
-    //     final response = phoneAuthServices.verifyOTP(event.otp);
-    //     _logger.i(response);
-    //     emit(AuthSuccess(id: '', displayName: '', email: '', message: 'OTP verified'));
-    //   } catch (e) {
-    //     emit(AuthFailure(e.toString()));
-    //   }
-    // });
-
     on<SignOut>((event, emit) async {
       await googleAuthServices.signOut();
-      emit(AuthPending());
+      emit(AuthNotSignedIn());
     });
   }
 }
