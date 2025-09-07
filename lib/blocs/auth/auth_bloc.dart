274304524edc024ea6a3e6110ccedbb60d5dd818
nailsonseat <aadarshv3@gmail.com>
2025-09-07 14:38:20 +0530

    choose role events

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -45,9 +45,10 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
         if (checkUserResponse.data == null) {
           try {
-            final createUserResponse = await userRepository.createUser(
-              {"name": response['displayName'], "email": response['email']},
-            );
+            final createUserResponse = await userRepository.createUser({
+              "name": response['displayName'],
+              "email": response['email'],
+            });
             if (createUserResponse.statusCode! >= 200) {
               lumenUser = LumenUser.fromJson({...createUserResponse.data, 'photoUrl': response['photoUrl']});
             } else {
@@ -64,7 +65,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           lumenUser = LumenUser.fromJson({...checkUserResponse.data[0], 'photoUrl': response['photoUrl']});
         }
 
-
         if (lumenUser.role == null) {
           Logger().w('User role is null for user: ${lumenUser.id}');
           emit(AuthSignedInAsAnonymous(lumenUser));
@@ -79,9 +79,10 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
           if (checkTeacherResponse.data == null) {
             try {
-              final createTeacherResponse = await teacherRepository.createTeacher(
-                {"name": response['displayName'], "email": response['email']},
-              );
+              final createTeacherResponse = await teacherRepository.createTeacher({
+                "name": response['displayName'],
+                "email": response['email'],
+              });
               if (createTeacherResponse.statusCode! >= 200) {
                 teacher = Teacher.fromJson(createTeacherResponse.data);
               } else {
@@ -111,9 +112,10 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
           if (checkStudentResponse.data == null) {
             try {
-              final createStudentResponse = await studentRepository.createStudent(
-                {"name": response['displayName'], "email": response['email']},
-              );
+              final createStudentResponse = await studentRepository.createStudent({
+                "name": response['displayName'],
+                "email": response['email'],
+              });
               if (createStudentResponse.statusCode! >= 200) {
                 student = Student.fromJson(createStudentResponse.data);
               } else {
@@ -162,6 +164,110 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         event.onSuccess?.call();
       } else {
         emit(AuthNotSignedIn());
+      }
+    });
+
+    on<ChooseTeacherRole>((event, emit) async {
+      emit(Loading());
+      // check of teacher exists else create
+      try {
+        final response = await teacherRepository.getAllTeachers({
+          "email": event.user.email,
+          "limit": "10",
+          "offset": "0",
+        });
+        Teacher? teacher;
+
+        if (response.data == null) {
+          try {
+            final createTeacherResponse = await teacherRepository.createTeacher({
+              "name": event.user.name,
+              "email": event.user.email,
+            });
+            if (createTeacherResponse.statusCode! >= 200) {
+              teacher = Teacher.fromJson(createTeacherResponse.data);
+            } else {
+              _logger.e('Failed to create teacher: ${createTeacherResponse.data}');
+              emit(AuthNotSignedIn());
+              return;
+            }
+          } catch (e) {
+            _logger.e('Error creating teacher: $e');
+            emit(AuthNotSignedIn());
+            return;
+          }
+        } else {
+          teacher = Teacher.fromJson(response.data[0]);
+        }
+
+        // update the user role to teacher
+        try {
+          final updateUserResponse = await userRepository.updateUser(event.user.copyWith(role: 'teacher'));
+          if (updateUserResponse.statusCode! >= 200) {
+            emit(
+              AuthSignedInAsTeacher(
+                user: event.user.copyWith(role: 'teacher'),
+                teacher: teacher,
+              ),
+            );
+            return;
+          } else {
+            _logger.e('Failed to update user role: ${updateUserResponse.data}');
+            emit(AuthNotSignedIn());
+            return;
+          }
+        } catch (e) {
+          _logger.e('Error choosing teacher role: $e');
+          emit(AuthNotSignedIn());
+          return;
+        }
+      } catch (e) {
+        _logger.e('Error choosing teacher role: $e');
+        emit(AuthNotSignedIn());
+        return;
+      }
+    });
+
+    on<ChooseStudentRole>((event, emit) async {
+      emit(Loading());
+      // check of student exists else create
+      try {
+        final response = await studentRepository.getStudents({
+          "email": event.user.email,
+          "limit": "10",
+          "offset": "0",
+          'extended': 'true',
+        });
+        Student? student;
+
+        if (response.data == null) {
+          try {
+            final createStudentResponse = await studentRepository.createStudent({
+              "name": event.user.name,
+              "email": event.user.email,
+            });
+            if (createStudentResponse.statusCode! >= 200) {
+              student = Student.fromJson(createStudentResponse.data);
+            } else {
+              _logger.e('Failed to create student: ${createStudentResponse.data}');
+              emit(AuthNotSignedIn());
+              return;
+            }
+          } catch (e) {
+            _logger.e('Error creating student: $e');
+            emit(AuthNotSignedIn());
+            return;
+          }
+        } else {
+          student = Student.fromJson(response.data[0]);
+        }
+
+        emit(AuthSignedInAsStudent(user: event.user, student: student));
+        return;
+      } catch (e) {
+        _logger.e('Error choosing student role: $e');
+        emit(AuthNotSignedIn());
+        return;
       }
     });
 
