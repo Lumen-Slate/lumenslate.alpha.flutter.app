3e288c95da1548ddd1cf5c8abc57f31e5c2825dd
nailsonseat <aadarshv3@gmail.com>
2025-09-07 14:47:22 +0530

    refactored file names

--- lib/pages/teacher_dashboard_page/teacher_dashboard.dart
+++ lib/pages/teacher_dashboard_page/teacher_dashboard.dart
@@ -3,16 +3,10 @@ import 'package:responsive_framework/responsive_framework.dart';
 import 'desktop/dashboard.dart';
 import 'mobile/dashboard.dart';
 
-class DashboardPage extends StatelessWidget {
-  const DashboardPage({super.key});
+class TeacherDashboardPage extends StatelessWidget {
+  const TeacherDashboardPage({super.key});
 
   @override
-  // Widget build(BuildContext context) {
-  //   bool isMobile = ResponsiveBreakpoints.of(context).isMobile;
-  //   return isMobile
-  //       ? TeacherDashboardMobile()
-  //       : TeacherDashboardDesktop();
-  // }
   Widget build(BuildContext context) {
     bool isMobile = ResponsiveBreakpoints.of(context).isMobile;
     return LayoutBuilder(
